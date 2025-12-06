336b928efb740342efd381ff85833c980f279d24
Majeed <aza523335@gmail.com>
2025-12-06 03:22:21 +0300

    Fix deployment errors

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,6 @@ import { defineConfig, env } from "@prisma/config";
 export default defineConfig({
   schema: "./prisma/schema.prisma",
   datasource: {
-    provider: "sqlite",
     url: env<{
       DATABASE_URL?: string;
     }>("DATABASE_URL"),
